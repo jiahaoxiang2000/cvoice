648f2163191798501c29e4c7e78b560ecbe0af47
isomo <jiahaoxiang2000@gmail.com>
2025-01-28 21:16:55 +0800

    Rename temporary directory from 'temp' to 'data' in FileHandler

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -1,12 +1,13 @@
 import os
 
+
 class FileHandler:
     @staticmethod
     def ensure_temp_dir():
-        temp_dir = "temp"
+        temp_dir = "data"
         os.makedirs(temp_dir, exist_ok=True)
         return temp_dir
 
     @staticmethod
     def get_temp_path(filename):
-        return os.path.join("temp", filename)
+        return os.path.join("data", filename)

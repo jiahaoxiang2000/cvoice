a22914b5afac9743b947a887469d8e2944c26dd0
isomo <jiahaoxiang2000@gmail.com>
2025-01-28 21:53:30 +0800

    Add logging functionality with rotating file handler and create logs directory

--- utils/logger.py
+++ utils/logger.py
@@ -1,18 +1,37 @@
 import logging
+import os
+from logging.handlers import RotatingFileHandler
+
 
 def setup_logger():
-    logger = logging.getLogger('cvoice')
+    # Create logs directory if it doesn't exist
+    logs_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs")
+    os.makedirs(logs_dir, exist_ok=True)
+
+    logger = logging.getLogger("cvoice")
     logger.setLevel(logging.INFO)
-    
-    # Create console handler with formatting
+
+    # Create formatter
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    # Console handler
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     console_handler.setFormatter(formatter)
-    
-    # Add handler to logger
+
+    # File handler
+    log_file = os.path.join(logs_dir, "cvoice.log")
+    file_handler = RotatingFileHandler(log_file, maxBytes=1024 * 1024, backupCount=5)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+
+    # Add handlers to logger
     logger.addHandler(console_handler)
-    
+    logger.addHandler(file_handler)
+
     return logger
+
 
 logger = setup_logger()

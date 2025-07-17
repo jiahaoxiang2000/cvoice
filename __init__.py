b50b6fd8445e880f08b7ce48188d0bfb375f35bc
jiahao xiang <jiahaoxiang2000@gmail.com>
2025-07-17 16:26:54 +0800

    Clear repository - starting fresh

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from .audio.extractor import AudioExtractor
-from .audio.synthesizer import AudioSynthesizer
-from .audio.merger import AudioMerger
-from .text.recognizer import TextRecognizer
-from .text.improver import TextImprover
-
-__version__ = "0.1.0"
